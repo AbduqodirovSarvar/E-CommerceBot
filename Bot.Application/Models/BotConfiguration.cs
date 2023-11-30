91399d40857f467386dfe731d4a30355aa4b182f
Sarvar Abduqodirov <abduqodirovsarvar.2002@gmail.com>
2023-11-30 16:49:58 +0500

    add some services

---
+++ Bot.Application/Models/BotConfiguration.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bot.Application.Models
+{
+    public class BotConfiguration
+    {
+        public static readonly string Configuration = "BotConfiguration";
+        public static readonly string RouteSection = "BotConfiguration:Route";
+        public string Token { get; set; } = string.Empty;
+        public string HostAddress { get; set; } = string.Empty;
+        public string AdminTelegramIds { get; set; } = string.Empty;
+        public string SecretKey { get; set; } = string.Empty;
+        public string Route { get; set; } = string.Empty;
+    }
+}
---
+++ Bot.Application/Services/HandleServices/CartService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bot.Application.Services.HandleServices
+{
+    internal class CartService
+    {
+
+    }
+}
